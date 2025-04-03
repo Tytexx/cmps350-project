4251925158ee3d0c69d45bbbd305343bd647c606
Mohammed Sherin-mv202212598 <mohammedsherinofficial@gmail.com>
2025-04-03 20:55:10 +0300

    PUSH login.js

--- js/login.js
+++ js/login.js
@@ -1,11 +1,32 @@
-const users = await fetch("../data/users.json");
-const userData = await users.json();
+async function userInput() {
+  const users = await fetch("../data/users.json");
+  const userData = await users.json();
 
-const loginForm = document.querySelector("#loginForm");
-const username = document.querySelector("#username");
-const password = document.querySelector("#password");
-const userType = document.querySelector("#userType");
+  const loginForm = document.querySelector("#loginForm");
+  const username = document.querySelector("#username");
+  const password = document.querySelector("#password");
+  //const userType = document.querySelector("#userType");
 
-function userInput() {
+  if (!loginForm) return;
 
+  loginForm.addEventListener("submit", function handleUserValidation(e) {
+    e.preventDefault();
+
+    console.log("Login form submitted");
+
+    const enteredUsername = username.value.trim();
+    const enteredPassword = password.value.trim();
+    //const selectedUserType = userType.value.trim();
+
+    const user = userData.find((user) => user.username === enteredUsername);
+
+    if (!user || user.password !== enteredPassword) {
+      alert("Incorrect Username or Password");
+      return;
+    }
+
+    alert(`Login Successful \nWelcome ${user.username}`);
+  });
 }
+
+userInput();
