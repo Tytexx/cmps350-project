04cda0b595fee5335ddd83e06fa2238c1e5cd3b4
Tytexx <151204083+Tytexx@users.noreply.github.com>
2025-04-07 17:01:34 +0300

    Changed number of credits to 3, more work on learningPath..js

--- js/learning-path.js
+++ js/learning-path.js
@@ -3,11 +3,16 @@ console.log("Hey");
 
 document.addEventListener("DOMContentLoaded",fetchData)
 const learningPathGrid = document.querySelector(".course-path-grid")
+const progressContainer = document.querySelector(".progress-stats")
 
 async function fetchData(){
     const courses = await fetch("../data/courses2.json");
     let courseList = await courses.json();
     localStorage.courses = JSON.stringify(courseList)
+
+    const students = await fetch("../data/students.json");
+    let studentList = await students.json();
+    localStorage.students = JSON.stringify(studentList)
     start()
 }
 
@@ -21,10 +26,32 @@ async function start(){
                                 </div>
 
         `)
-
+        loadProgramProgress()
                           
 }
 
+async function loadProgramProgress(){
+    const completedCourses = JSON.parse(localStorage.studentList).map(student => student.courses).reduce((sum,course), sum + course, 0);
+    const completedCourse = "";
+
+    progressContainer.innerHTML = `<div class="stat-item">
+<span class="stat-label">Completed Courses:</span>
+<span class="stat-value" id="completed-courses">9/20</span>
+</div>
+<div class="stat-item">
+<span class="stat-label">Credits Earned:</span>
+<span class="stat-value" id="credits-earned">36/100</span>
+</div>
+<div class="stat-item">
+<span class="stat-label">Current GPA:</span>
+<span class="stat-value" id="current-gpa">3.75</span>
+</div>`
+    
+
+                          
+}
+{}
 // #28a745
 // #ffc107
 // #355c7d
+
