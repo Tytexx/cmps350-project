da2075e5be8efdde367e106a7416890a5debc755
aman-am2309156 <am2309156@qu.edu.qa>
2025-04-08 12:19:22 +0300

    made changes to structure

--- js/main.js
+++
@@ -1,8 +0,0 @@
-// const BASE_URL = 'http://127.0.0.1:3000/cmps350-project/home.html'
-// import * as home from 'js/home.js';
-
-// document.addEventListener("DOMContentLoaded",home.fetchData);
-
-// home.fetchData();
-
-
